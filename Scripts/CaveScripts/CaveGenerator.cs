4acfce624b0a04e0395f29199525b77ddfc12b98
Robert <rob.staples91@gmail.com>
2017-11-13 15:40:38 +1100

    Added region control

--- Scripts/CaveScripts/CaveGenerator.cs
+++ Scripts/CaveScripts/CaveGenerator.cs
@@ -35,6 +35,8 @@ public class CaveGenerator : MonoBehaviour {
 			SmoothMap ();
 		}
 
+		ProcessMap();
+
 		int borderSize = border;
 		int [,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2]
 
@@ -52,6 +54,81 @@ public class CaveGenerator : MonoBehaviour {
 		CaveMesh meshGen = GetComponent<CaveMesh> ();
 		meshGen.GenerateMesh (borderedMap, 1);
 	}
+
+void ProcessMap() {
+	List<List<Coord>> wallRegions = GetRegions(1);
+
+	//make public
+	int wallThreasholdSize = 50;
+	foreach (List<Coord> wallRegion in wallRegions) {
+		if (wallRegion.Count < wallThreasholdSize) {
+			foreach (Coord tile in wallRegion) {
+				map[tile.tileX, tile.tileY] = 0;
+			}
+		}
+	}
+
+	List<List<Coord>> roomRegions = GetRegions(0);
+
+	//make public
+	int roomThreasholdSize = 50;
+	foreach (List<Coord> roomRegion in roomRegions) {
+		if (roomRegion.Count < roomThreasholdSize) {
+			foreach (Coord tile in roomRegion) {
+				map[tile.tileX, tile.tileY] = 1;
+			}
+		}
+	}
+}
+
+List<List<Coord>> GetRegions(int tileType) {
+	List<List<Coord>> regions = new List<List<Coord>>();
+	int[,] mapFlags = new int[width,height];
+
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			if (mapFlags[x,y] == 0 && map[x,y] == tileType) {
+				List<Coord> newRegion = GetRegionTiles(x,y);
+				regions.Add(newRegion);
+				foreach (Coord tile in newRegion) {
+					mapFlags[tile.tileX, tile.tileY] = 1;
+				}
+			}
+		}
+	}
+	return regions;
+}
+
+	List<Coord> GetRegionTiles(int startX, int startY) {
+		List<Coord> tiles = new List<Coord>();
+		int[,] mapFlags = new int[width,height];
+		int tileType = map[startX,startY];
+
+		Queue<Coord> queue = new Queue<Coord>();
+		queue.Enqueue(new Coord[startX, startY]);
+		mapFlags[startX, startY] = 1;
+
+		while (queue.Count > 0) {
+			Coord tile = queue.Dequeue();
+			tiles.Add(tile);
+
+			for (int x = tile.tileX -1; x <= tile.tileX +1; x++) {
+				for (int y = tile.tileY -1; y <= tile.tileY +1; y++) {
+					if (IsInMapRange(x,y) && (y == tile.tileY || x == tile.tileX)) {
+						if (mapFlags[x,y] == 0 && map[x,y] == tileType) {
+							mapFlags[x,y] == 1;
+							queue.Enqueue(new Coord(x,y));
+						}
+					}
+			}
+		}
+		return tiles;
+	}
+
+	bool IsInMapRange(int x, int y) {
+		return x >= 0 && x < width && y >= 0 && y < height;
+	}
+
 	void RandomFillMap(){
 		if (useRandomSeed) {
 			seed = Time.time.ToString ();
@@ -88,7 +165,7 @@ public class CaveGenerator : MonoBehaviour {
 		int wallCount = 0;
 		for (int neighbourX = gridX -1; neighbourX <= gridX + 1; neighbourX++){
 			for (int neighbourY = gridY -1; neighbourY <= gridY + 1; neighbourY++){
-				if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height) {
+				if (IsInMapRange(neighbourX, neighbourY)) {
 					if (neighbourX != gridX || neighbourY != gridY) {
 						wallCount += map [neighbourX, neighbourY];
 					}
@@ -100,15 +177,13 @@ public class CaveGenerator : MonoBehaviour {
 		return wallCount;
 	}
 
-//	void OnDrawGizmos() {
-//		if (map != null) {
-//			for (int x = 0; x < width; x++) {
-//				for (int y = 0; y < height; y++) {
-//					Gizmos.color = (map [x, y] == 1) ? Color.black : Color.white;
-//					Vector3 pos = new Vector3 (-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-//					Gizmos.DrawCube (pos, Vector3.one);
-//				}
-//			}
-//		}
-//	}
+	struct Cood {
+		public int tileX;
+		public int tileY;
+
+		public Coord(int x, int y) {
+			tileX = X;
+			tileY = Y;
+		}
+	}
 }
