04ae08516c6813033eb893e0931d1b7ea63d614d
Robert <rob.staples91@gmail.com>
2017-11-15 16:22:19 +1100

    started the editor tooling

---
+++ Editor/ShapeEditor.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEditor;
+﻿using UnityEngine;
+
+[CustomEditor (typeof (ShapeCreator))]
+public class ShapeEditor : Editor{
+
+  ShapeCreator shapeCreator;
+  bool needsRepaint;
+
+  void onSceneGUI() {
+    Event guiEvent = Event.Current;
+
+    if (guiEvent.type == EventType.Repaint) {
+      Draw();
+    } else if (guiEvent.type == EventType.layout) {
+      HandleUtility.AddDefultcontorl(GUI.GetControlId(FocusType.Passive));
+    } else {
+      HandleInput(guiEvent);
+      if (needsRepaint) {
+        HandleUtility.Repaint();
+      }
+    }
+  }
+
+  void HandleInput(Event guiEvent) {
+    Ray mouseRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+    float drawPlaneHeight = 0;
+    float dstToDrawPlane = (drawPlaneHeight - mouseRay.origin.y)/mouseRay.direction.y;
+    Vector3 mousePosition = mouseRay.GetPoint(dstToDrawPlane);
+
+    if (guiEvent.type == EventType.mouseDown && guiEvent.button == 0 && guiEvent.modifiers == EventModifiers.none) {
+      Undo.RecordObject(ShapeCreator, "Add Point");
+      shapeCreator.points.Add(mousePosition);
+      needsRepaint = true;
+    }
+  }
+
+  void Draw() {
+    for (int i = 0; i< shapeCreator.points.Count; i++) {
+      Vector3 nextPoint = shapeCreator.points[(i + 1)  % shapeCreator.points.Count];
+      Handles.Color = Color.Black;
+      Handles.DrawDottedLine(shapeCreator.points[i], nextPoint, 4);
+      Handles.Color = Color.White;
+      Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, .5f);
+      needsRepaint = false;
+    }
+  }
+
+  void OnEnable() {
+    shapeCreator = target as ShapeCreator;
+  }
+}
---
+++ Scripts/ShapeCreator.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Generic;
+using Unity;
+
+
+public class ShapeCreator : MonoBehaviour{
+
+  [HideInEditor]
+  public List<Vector3> points = new List<Vector3>();
+
+}
