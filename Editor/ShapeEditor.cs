efd4874aa728b0002009892b294e9f55d10c533b
robert <rob.staples91@gmail.com>
2017-11-20 22:59:34 +1100

    Added move and create functionality

--- Editor/ShapeEditor.cs
+++ Editor/ShapeEditor.cs
@@ -2,53 +2,188 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEditor;
 using UnityEngine;
+[CustomEditor(typeof(ShapeCreator))]
+public class ShapeEditor : Editor
+{
 
-[CustomEditor (typeof (ShapeCreator))]
-public class ShapeEditor : Editor{
+    ShapeCreator shapeCreator;
+    SelectionInfo selectionInfo;
+    bool needsRepaint;
 
-  ShapeCreator shapeCreator;
-  bool needsRepaint;
+    void OnSceneGUI()
+    {
+        Event guiEvent = Event.current;
 
-  void onSceneGUI() {
-    Event guiEvent = Event.current;
-
-    if (guiEvent.type == EventType.Repaint) {
-      Draw();
-    } else if (guiEvent.type == EventType.layout) {
-      HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
-    } else {
-      HandleInput(guiEvent);
-      if (needsRepaint) {
-        HandleUtility.Repaint();
-      }
+        if (guiEvent.type == EventType.Repaint)
+        {
+            Draw();
+        }
+        else if (guiEvent.type == EventType.Layout)
+        {
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+        }
+        else
+        {
+            HandleInput(guiEvent);
+            if (needsRepaint)
+            {
+                HandleUtility.Repaint();
+            }
+        }
     }
-  }
 
-  void HandleInput(Event guiEvent) {
-    Ray mouseRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
-    float drawPlaneHeight = 0;
-    float dstToDrawPlane = (drawPlaneHeight - mouseRay.origin.y)/mouseRay.direction.y;
-    Vector3 mousePosition = mouseRay.GetPoint(dstToDrawPlane);
+    void HandleInput(Event guiEvent)
+    {
+        Ray mouseRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+        float drawPlaneHeight = 0;
+        float dstToDrawPlane = (drawPlaneHeight - mouseRay.origin.y) / mouseRay.direction.y;
+        Vector3 mousePosition = mouseRay.GetPoint(dstToDrawPlane);
 
-    if (guiEvent.type == EventType.mouseDown && guiEvent.button == 0 && guiEvent.modifiers == EventModifiers.None) {
-      Undo.RecordObject(shapeCreator, "Add Point");
-      shapeCreator.points.Add(mousePosition);
-      needsRepaint = true;
+        if (guiEvent.type == EventType.mouseDown && guiEvent.button == 0 && guiEvent.modifiers == EventModifiers.None)
+        {
+            HandleLeftMouseDown(mousePosition);
+        }
+        if (guiEvent.type == EventType.mouseUp && guiEvent.button == 0 && guiEvent.modifiers == EventModifiers.None)
+        {
+            HandleLeftMouseUp(mousePosition);
+        }
+        if (guiEvent.type == EventType.mouseDrag && guiEvent.button == 0 && guiEvent.modifiers == EventModifiers.None)
+        {
+            HandleLeftMouseDrag(mousePosition);
+        }
+        if (!selectionInfo.isSelected)
+        {
+            UpdateMouseOverInfo(mousePosition);
+        }
     }
-  }
 
-  void Draw() {
-    for (int i = 0; i< shapeCreator.points.Count; i++) {
-      Vector3 nextPoint = shapeCreator.points[(i + 1)  % shapeCreator.points.Count];
-      Handles.color = Color.black;
-      Handles.DrawDottedLine(shapeCreator.points[i], nextPoint, 4);
-      Handles.color = Color.white;
-      Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, .5f);
-      needsRepaint = false;
+    void HandleLeftMouseDown(Vector3 mousePosition)
+    {
+        if (!selectionInfo.mouseIsOverPoint)
+        {
+            int newPointIndex = (selectionInfo.mouseIsOverLine) ? selectionInfo.lineIndex + 1 : shapeCreator.points.Count;
+            Undo.RecordObject(shapeCreator, "Add Point");
+            shapeCreator.points.Insert(newPointIndex, mousePosition);
+            selectionInfo.pointIndex = newPointIndex;
+        }
+
+        selectionInfo.isSelected = true;
+        needsRepaint = true;
+        selectionInfo.positionAtStartOfDrag = mousePosition;
     }
-  }
 
-  void OnEnable() {
-    shapeCreator = target as ShapeCreator;
-  }
+    void HandleLeftMouseUp(Vector3 mousePosition)
+    {
+        if (selectionInfo.isSelected)
+        {
+            shapeCreator.points[selectionInfo.pointIndex] = selectionInfo.positionAtStartOfDrag;
+            Undo.RecordObject(shapeCreator, "Move Point");
+            shapeCreator.points[selectionInfo.pointIndex] = mousePosition;
+            selectionInfo.isSelected = false;
+            selectionInfo.pointIndex = -1;
+            needsRepaint = true;
+        }
+    }
+
+    void HandleLeftMouseDrag(Vector3 mousePosition)
+    {
+        if (selectionInfo.isSelected)
+        {
+            shapeCreator.points[selectionInfo.pointIndex] = mousePosition;
+            needsRepaint = true;
+        }
+    }
+
+    void UpdateMouseOverInfo(Vector3 mousePosition)
+    {
+        int mouseOverPointIndex = -1;
+        for (int i = 0; i < shapeCreator.points.Count; i++)
+        {
+            if (Vector3.Distance(mousePosition, shapeCreator.points[i]) < shapeCreator.handleRadius)
+            {
+                mouseOverPointIndex = i;
+                break;
+            }
+        }
+
+        if (mouseOverPointIndex != selectionInfo.pointIndex)
+        {
+            selectionInfo.pointIndex = mouseOverPointIndex;
+            selectionInfo.mouseIsOverPoint = mouseOverPointIndex != -1;
+            needsRepaint = true;
+        }
+        if (selectionInfo.mouseIsOverPoint)
+        {
+            selectionInfo.mouseIsOverLine = false;
+            selectionInfo.lineIndex = -1;
+        }
+        else
+        {
+            int mouseOverLineIndex = -1;
+            float closestLineDst = shapeCreator.handleRadius;
+            for (int i = 0; i < shapeCreator.points.Count; i++)
+            {
+                Vector3 nextPointInShape = shapeCreator.points[(i + 1) % shapeCreator.points.Count];
+                float dstFromMouseToLine = HandleUtility.DistancePointToLineSegment(mousePosition.ToXZ(), shapeCreator.points[i].ToXZ(), nextPointInShape.ToXZ());
+                if (dstFromMouseToLine < closestLineDst)
+                {
+                    closestLineDst = dstFromMouseToLine;
+                    mouseOverLineIndex = i;
+                }
+            }
+
+            if (selectionInfo.lineIndex != mouseOverLineIndex)
+            {
+                selectionInfo.lineIndex = mouseOverLineIndex;
+                selectionInfo.mouseIsOverLine = mouseOverLineIndex != -1;
+                needsRepaint = true;
+            }
+        }
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < shapeCreator.points.Count; i++)
+        {
+            Vector3 nextPoint = shapeCreator.points[(i + 1) % shapeCreator.points.Count];
+            if (i == selectionInfo.lineIndex)
+            {
+                Handles.color = Color.red;
+                Handles.DrawLine(shapeCreator.points[i], nextPoint);
+            }
+            else
+            {
+                Handles.color = Color.black;
+                Handles.DrawDottedLine(shapeCreator.points[i], nextPoint, 4);
+            }
+            if (i == selectionInfo.pointIndex)
+            {
+                Handles.color = (selectionInfo.isSelected) ? Color.black : Color.red;
+            }
+            else
+            {
+                Handles.color = Color.white;
+            }
+
+            Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, shapeCreator.handleRadius);
+            needsRepaint = false;
+        }
+    }
+
+    void OnEnable()
+    {
+        shapeCreator = target as ShapeCreator;
+        selectionInfo = new SelectionInfo();
+    }
+
+    public class SelectionInfo
+    {
+        public int pointIndex;
+        public bool mouseIsOverPoint;
+        public bool isSelected;
+        public Vector3 positionAtStartOfDrag;
+
+        public int lineIndex = -1;
+        public bool mouseIsOverLine;
+    }
 }
---
+++ Scripts/ShapeTool/ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods {
+    public static Vector2 ToXZ (this Vector3 V3)
+    {
+        return new Vector2(V3.x, V3.y);
+    }
+}
